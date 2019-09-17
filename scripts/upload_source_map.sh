9025dcfd7c23029a208742bcf0b6166c22b335ca
Josh Salisbury <josh@adhocteam.us>
2019-09-17 15:18:03 -0500

    Set rewrite flag to true when uploading maps to sentry (#182)

--- scripts/upload_source_map.sh
+++ scripts/upload_source_map.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sentry-cli releases new $SENTRY_RELEASE
-sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps $SOURCE_DIR --url-prefix '~/static/js'
+sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps $SOURCE_DIR --url-prefix '~/static/js' --rewrite
 sentry-cli releases finalize $SENTRY_RELEASE
