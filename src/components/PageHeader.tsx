53c069d72a5c40467d97ba0efaab4d26f9743eb9
Mike Lumetta <mike.lumetta@adhocteam.us>
2019-10-16 17:04:52 -0400

    Use VADS in PageHeader (#230)
    * Use VADS in PageHeader
    
    * Update screenshots for PageHeader

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -14,17 +14,17 @@ interface IPageHeaderProps {
 export default class PageHeader extends React.Component<IPageHeaderProps, {}> {
   public render() { 
     return (
-      <div className={classNames('va-api-page-header', this.props.className)}>
+      <div className={this.props.className}>
         {this.props.halo &&
-          <div className="header-halo">
+          <div className={classNames('header-halo', 'vads-u-color--gray')}>
             {this.props.halo}
           </div>
         }
-        <h1 id={this.props.id}>
+        <h1 id={this.props.id} className={classNames('vads-u-margin-top--0', 'vads-u-margin-bottom--2')}>
           {this.props.header}
         </h1>
         {this.props.description &&
-          <h2>
+          <h2 className={classNames('vads-u-font-size--lg')}>
             {this.props.description}
           </h2>
         }
