b9013d60d3fec532e3b34a31f4cd7382c2c85e3b
Nick Fasulo <nrfasulo@gmail.com>
2020-04-28 08:31:16 -0500

    Add tests of cached application state (#338)
    * Add tests for loadApplicationState
    
    * Use dynamic imports to avoid require()

---
+++ src/store.test.ts
@@ -0,0 +1,34 @@
+import { initialApplicationState } from './reducers';
+
+// sessionStorage.getItem('state') is called when the store is imported at application start up.
+// To be able to pass in artibray values for testing purposes, the store is reset between tests 
+// and then initialized after setting sessionStorage.
+
+beforeEach(() => {
+  sessionStorage.clear();
+  jest.resetModules();
+});
+
+describe('loadApplicationState', () => {
+  it('returns a blank application when sessionStorage is empty', async () => {
+    const store = (await import('./store')).default;
+    const state = store.getState();
+    expect(state.application).toEqual(initialApplicationState);
+  });
+
+  it('returns the cached application when available', async () => {
+    const savedState = '{"application":{"inputs":{"apis":{"appeals":false,"benefits":false,"claims":false,"communityCare":false,"confirmation":false,"facilities":false,"health":false,"vaForms":false,"verification":false},"description":{"dirty":false,"value":""},"email":{"dirty":false,"value":""},"firstName":{"dirty":false,"value":"Test"},"lastName":{"dirty":false,"value":"User"},"oAuthApplicationType":{"dirty":false,"value":""},"oAuthRedirectURI":{"dirty":false,"value":""},"organization":{"dirty":false,"value":""},"termsOfService":false}}}';
+    sessionStorage.setItem('state', savedState);
+    const store = (await import('./store')).default;
+    const state = store.getState();
+    expect(state.application).toEqual(JSON.parse(savedState).application);
+  });
+
+  it('returns a blank application when the form schema has changed', async () => {
+    const saveState = '{"application":{"inputs":{"apis":{"appeals":false,"benefits":false,"claims":false,"communityCare":false,"confirmation":false,"facilities":false,"health":false,"vaForms":false,"verification":false},"description":{"dirty":false,"value":""},"email":{"dirty":false,"value":""},"firstName":{"dirty":false,"value":"Test"},"middleName":{"dirty":false,"value":"This"},"lastName":{"dirty":false,"value":"User"},"oAuthApplicationType":{"dirty":false,"value":""},"oAuthRedirectURI":{"dirty":false,"value":""},"organization":{"dirty":false,"value":""},"termsOfService":false}}}';
+    sessionStorage.setItem('state', saveState);
+    const store = (await import('./store')).default;
+    const state = store.getState();
+    expect(state.application).toEqual(initialApplicationState);
+  });
+});
