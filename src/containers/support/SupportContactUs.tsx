b5e45d6bcd8d362c7a5ba44a37842046db77d32e
johanna-adhoc <55552374+johanna-adhoc@users.noreply.github.com>
2020-09-28 12:43:28 -0400

    Migrated SupportContactUs component. (#441)

--- src/containers/support/SupportContactUs.tsx
+++ src/containers/support/SupportContactUs.tsx
@@ -1,57 +1,51 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import * as React from "react";
-import PageHeader from "../../components/PageHeader";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as React from 'react';
+import PageHeader from '../../components/PageHeader';
 import SupportConfirmation from '../../content/supportConfirmation.mdx';
-
 import SupportContactUsForm from './SupportContactUsForm';
 
-const GitHubSnippet = () => {
-  return (
-    <div className="vads-u-margin-y--2">
-      <h3>Submit an Issue via GitHub</h3>
-      <a className="usa-button" href="https://github.com/department-of-veterans-affairs/vets-api-clients/issues/new/choose">
-        <FontAwesomeIcon icon={faGithub} /> Submit an Issue
-      </a>
-    </div>
-  );
+const GitHubSnippet = () => (
+  <div className="vads-u-margin-y--2">
+    <h3>Submit an Issue via GitHub</h3>
+    <a
+      className="usa-button"
+      href="https://github.com/department-of-veterans-affairs/vets-api-clients/issues/new/choose"
+    >
+      <FontAwesomeIcon icon={faGithub} /> Submit an Issue
+    </a>
+  </div>
+);
+
+const headerProps = {
+  description:
+    'You can submit an issue on GitHub (requires an account) or contact us by using the form below. Please provide as much detail as possible. Your concerns and feedback are important to us, and you can expect a reply within one business day.',
+  halo: 'Support',
+  header: 'Contact Us',
 };
 
-interface ISupportContactUsState {
-  sent: boolean;
-}
+const SupportContactUs = (): JSX.Element => {
+  const [sent, setSent] = React.useState(false);
 
-export default class SupportContactUs extends React.Component<{}, ISupportContactUsState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      sent: false,
-    };
+  const onSuccess = () => {
+    setSent(true);
+  };
 
-    this.onSuccess = this.onSuccess.bind(this);
-  }
-
-  public render() {
-    const headerProps = {
-      description: "You can submit an issue on GitHub (requires an account) or contact us by using the form below. Please provide as much detail as possible. Your concerns and feedback are important to us, and you can expect a reply within one business day.",
-      halo: "Support",
-      header: "Contact Us",
-    };
-
-    if (this.state.sent) {
-      return <SupportConfirmation />;
-    } else {
-      return (
+  /**
+   * RENDER
+   */
+  return (
+    <>
+      {sent && <SupportConfirmation />}
+      {!sent && (
         <section role="region" aria-label="Support Overview">
           <PageHeader {...headerProps} />
           <GitHubSnippet />
-          <SupportContactUsForm onSuccess={this.onSuccess}/>
+          <SupportContactUsForm onSuccess={onSuccess} />
         </section>
-      );
-    }
-  }
+      )}
+    </>
+  );
+};
 
-  private onSuccess(): void {
-    this.setState({sent: true});
-  }
-}
+export default SupportContactUs;
