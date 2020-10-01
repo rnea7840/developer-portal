9caa32729a044240509bf7f0be5a164bd37c1e59
johanna-adhoc <55552374+johanna-adhoc@users.noreply.github.com>
2020-10-01 11:52:46 -0400

    API-2535: Hooks Migration App and AuthorizationCard (#445)
    * Migrated components App and AuthorizationCard
    
    * Fixed interface naming convention.
    
    * Added comment back.
    
    Co-authored-by: Anthony Kopczyk <47796360+anthony-kopczyk@users.noreply.github.com>

---
+++ src/components/authorizationCard/AuthorizationCard.tsx
@@ -0,0 +1,21 @@
+import * as PropTypes from 'prop-types';
+import * as React from 'react';
+
+import CardLink from '../CardLink';
+
+const AuthorizationCardPropTypes = {
+  categoryKey: PropTypes.string.isRequired,
+};
+
+type AuthorizationCardProps = PropTypes.InferProps<typeof AuthorizationCardPropTypes>;
+
+const AuthorizationCard = (props: AuthorizationCardProps): JSX.Element => (
+  <CardLink name="Authorization" url={`/explore/${props.categoryKey}/docs/authorization`}>
+    Use the OpenID Connect standard to allow Veterans to authorize third-party application to access
+    data on their behalf.
+  </CardLink>
+);
+
+AuthorizationCard.propTypes = AuthorizationCardPropTypes;
+
+export { AuthorizationCard };
