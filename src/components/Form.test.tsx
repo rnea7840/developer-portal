08c71bb1ad8f10579f3bc425e4704c340f98b1d2
Mike Lumetta <mike.lumetta@adhocteam.us>
2020-10-02 15:27:31 -0400

    Refactor Form submit error test to use React Testing Library to correctly handle async onSubmit

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,3 +1,5 @@
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { mount } from 'enzyme';
 import 'jest';
 import * as React from 'react';
@@ -16,18 +18,20 @@ describe('Form', () => {
     expect(onSuccessMock.mock.calls.length).toEqual(0);
   });
 
-  it('should display the error alert when in error', () => {
-    const onSubmitMock = jest.fn().mockImplementation(() => {
-      throw new Error();
-    });
+  it('should display the error alert when in error', async () => {
+    const onSubmitMock = jest.fn().mockRejectedValue(new Error());
     const onSuccessMock = jest.fn();
 
-    const component = mount(
-      <Form onSubmit={onSubmitMock} onSuccess={onSuccessMock} disabled={false} />,
-    );
-    component.find('.usa-button-primary').simulate('click');
+    render(<Form onSubmit={onSubmitMock} onSuccess={onSuccessMock} disabled={false} />);
 
-    expect(component.find('.usa-alert-error').length).toEqual(1);
+    const submitButton = screen.getByRole('button', { name: 'Submit' });
+    fireEvent.click(submitButton);
+
+    const errorMessage = await screen.findByRole('heading', {
+      name: 'We encountered a server error while saving your form. Please try again later.',
+    });
+
+    expect(errorMessage).toBeInTheDocument();
     expect(onSuccessMock.mock.calls.length).toEqual(0);
   });
 
