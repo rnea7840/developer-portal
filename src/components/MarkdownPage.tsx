3a463ae730a45f6241ab2da4602696b2701a4aef
Mike Lumetta <mike.lumetta@adhocteam.us>
2020-10-02 10:37:56 -0400

    Fix ESLint errors in MarkdownPage

--- src/components/MarkdownPage.tsx
+++ src/components/MarkdownPage.tsx
@@ -1,12 +1,13 @@
-import MarkdownComponent from '*.mdx';
 import * as React from 'react';
 
-export default function MarkdownPage(Component: MarkdownComponent): JSX.Element {
-  return (
-    <section className="vads-u-padding-y--5">
-      <div className="vads-l-grid-container">
-        <Component />
-      </div>
-    </section>
-  );
-}
+import MarkdownComponent from '*.mdx';
+
+const MarkdownPage = (Component: MarkdownComponent): JSX.Element => (
+  <section className="vads-u-padding-y--5">
+    <div className="vads-l-grid-container">
+      <Component />
+    </div>
+  </section>
+);
+
+export default MarkdownPage;
