9b258fc8642b2eb3fef6964e44f0a8724bfdf768
Anthony Kopczyk <47796360+anthony-kopczyk@users.noreply.github.com>
2020-10-05 15:24:44 -0400

    Api 2546: Hook migration for banner (#447)
    * Convert Banner to use react hooks
    
    * Add mocks for images for unit tests
    
    With typescript and requiring properties to have values, I found images
    were being imported as undefined within unit tests. This fixes unit
    tests. I still need to look into other tests. I'm particularly concerned
    there might be an issue with visual regression tests, but we'll find
    out!
    
    * Add alt text property to icon
    
    The icons in the GuidanceBox component all had the same alt text. This
    adds an optional icon_alt property to set the alt text on the given
    icon. It's optional so that if the icon is purely decorative, we can
    leave the alt text off.

--- src/components/Banner.test.tsx
+++ src/components/Banner.test.tsx
@@ -2,33 +2,46 @@ import * as React from 'react';
 
 import 'jest';
 
-import { shallow } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import Banner from './Banner';
 
 describe('Banner', () => {
+  let wrapper: ReactWrapper<unknown, unknown, React.Component>;
+
+  beforeEach(() => {
+    wrapper = mount(<Banner />);
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
   it('should render the site notice text', () => {
-    const wrapper = shallow(<Banner />);
     expect(wrapper.find('.site-notice-text').length).toBe(1);
     expect(wrapper.find('.site-notice-text')
       .contains('An official website of the United States government.')).toBeTruthy();
   });
 
   it('should render the dot gov guidance', () => {
-    expect(shallow(<Banner />).find('#dot-gov-guidance').length).toBe(1);
+    expect(wrapper.find('#dot-gov-guidance').hostNodes().length).toEqual(1);
   });
 
   it('should render the HTTPS guidance', () => {
-    expect(shallow(<Banner />).find('#https-guidance').length).toBe(1);
+    expect(wrapper.find('#https-guidance').hostNodes().length).toEqual(1); 
   });
 
   it('should not show the site guidance accordion by default', () => {
-    const accordionWrapper = shallow(<Banner />).find('.usa-accordion-content');
+    const accordionWrapper = wrapper.find('.usa-accordion-content');
     expect(accordionWrapper.prop('aria-hidden')).toBe('true');
   });
 
-  it('should show the site guidance accordion when accordionVisible is true', () => {
-    const wrapper = shallow(<Banner />);
-    wrapper.setState({ accordionVisible: true });
+  it('should toggle the site guidance accordion when the guidance accordion toggle is clicked', () => {
+    const toggleButtonWrapper = wrapper.find('#toggle-how-you-know-dropdown');
+
+    toggleButtonWrapper.simulate('click');
     expect(wrapper.find('.usa-accordion-content').prop('aria-hidden')).toBe('false');
+    
+    toggleButtonWrapper.simulate('click');
+    expect(wrapper.find('.usa-accordion-content').prop('aria-hidden')).toBe('true');
   });
 });
