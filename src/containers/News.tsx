3ad0017f55774388307e98a45e03e67fcee721ae
TS000 <tylersnell@me.com>
2020-10-05 08:31:21 -0600

    Converted Class to Hooks and Function to Hooks

--- src/containers/News.tsx
+++ src/containers/News.tsx
@@ -34,11 +34,10 @@ const sections = NewsData.sections.map((section: DataSection) => ({
   id: toHtmlId(section.title),
 }));
 
-function NewsItem({ item, media }: { item: NewsItem; media: boolean }) {
-  return media ? <MediaItem item={item} /> : <ItemDescription item={item} />;
-}
+const NewsItem = ({ item, media }: { item: NewsItem; media: boolean }) =>
+  media ? <MediaItem item={item} /> : <ItemDescription item={item} />;
 
-function MediaItem({ item }: { item: NewsItem }) {
+const MediaItem = ({ item }: { item: NewsItem }) => {
   const description = <ItemDescription item={item} />;
   if (item.url.includes('www.youtube.com')) {
     return (
@@ -62,22 +61,20 @@ function MediaItem({ item }: { item: NewsItem }) {
       <div className="vads-u-margin-left--2p5 va-api-media-row-description">{description}</div>
     </div>
   );
-}
+};
 
-function ItemDescription({ item }: { item: NewsItem }) {
-  return (
-    <p>
-      <a href={item.url}>{item.title}</a>
-      <br />
-      <strong>
-        {item.date}
-        {item.source ? ` | ${item.source}` : null}
-      </strong>
-    </p>
-  );
-}
+const ItemDescription = ({ item }: { item: NewsItem }) => (
+  <p>
+    <a href={item.url}>{item.title}</a>
+    <br />
+    <strong>
+      {item.date}
+      {item.source ? ` | ${item.source}` : null}
+    </strong>
+  </p>
+);
 
-export default function News() {
+const News = () => {
   const pageDescription =
     'This page is where you’ll find interesting press releases, articles, or media that relate to the VA Lighthouse program and the Developer Portal.';
 
@@ -87,16 +84,14 @@ export default function News() {
         <div className="vads-l-row">
           <SideNav ariaLabel="News Side Nav">
             <SideNavEntry key="all" exact={true} to="/news" name="Overview" />
-            {sections.map((section: any) => {
-              return (
-                <SideNavEntry
-                  key={section.id}
-                  to={`#${section.id}`}
-                  name={section.title}
-                  onClick={onHashAnchorClick}
-                />
-              );
-            })}
+            {sections.map((section: any) => (
+              <SideNavEntry
+                key={section.id}
+                to={`#${section.id}`}
+                name={section.title}
+                onClick={onHashAnchorClick}
+              />
+            ))}
           </SideNav>
           <div className={classNames('vads-l-col--12', 'medium-screen:vads-l-col--8')}>
             <section role="region" aria-label="News">
@@ -106,34 +101,32 @@ export default function News() {
                 className="vads-u-margin-bottom--4"
               />
               <div className={classNames(defaultFlexContainer(), 'vads-u-margin-bottom--4')}>
-                {sections.map((section: NewsSection) => {
-                  return (
-                    <CardLink key={section.id} url={`#${section.id}`} name={section.title}>
-                      {section.description}
-                    </CardLink>
-                  );
-                })}
+                {sections.map((section: NewsSection) => (
+                  <CardLink key={section.id} url={`#${section.id}`} name={section.title}>
+                    {section.description}
+                  </CardLink>
+                ))}
               </div>
-              {sections.map((section: NewsSection) => {
-                return (
-                  <section
-                    aria-label={section.title}
-                    key={section.id}
-                    className="vads-u-margin-bottom--4"
-                  >
-                    <h2 id={section.id} tabIndex={-1}>
-                      {section.title}
-                    </h2>
-                    {section.items.map((item: NewsItem) => {
-                      return <NewsItem key={item.url} item={item} media={section.media} />;
-                    })}
-                  </section>
-                );
-              })}
+              {sections.map((section: NewsSection) => (
+                <section
+                  aria-label={section.title}
+                  key={section.id}
+                  className="vads-u-margin-bottom--4"
+                >
+                  <h2 id={section.id} tabIndex={-1}>
+                    {section.title}
+                  </h2>
+                  {section.items.map((item: NewsItem) => (
+                    <NewsItem key={item.url} item={item} media={section.media} />
+                  ))}
+                </section>
+              ))}
             </section>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default News;
