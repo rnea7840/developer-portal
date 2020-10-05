a109514709cf4759f30a52499c94ac777ea6c51f
Giles Wells <giles.wells@oddball.io>
2020-10-05 11:04:36 -0400

    API-1761: Remove beta footer bar (#454)
    * Remove footer beta bar and update unit tests
    
    * Updated visual screenshots without beta footer
    
    * Testing double the timeout
    
    * e2e boosted timeout as well

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -6,10 +6,8 @@ import { shallow } from 'enzyme';
 import Footer from './Footer';
 
 describe('Footer', () => {
-  it('should render the site disclaimer', () => {
+  it('There should be no more beta footer banner', () => {
     const wrapper = shallow(<Footer />);
-    expect(wrapper.find('.va-api-beta-banner').length).toBe(1);
-    expect(wrapper.find('.va-api-beta-banner').text()
-      .includes('This is a beta site')).toBeTruthy();
+    expect(wrapper.find('.va-api-beta-banner').length).toBe(0);
   });
 });
