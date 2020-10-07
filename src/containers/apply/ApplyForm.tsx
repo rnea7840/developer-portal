ee4d400a6e63dac1f7ca6de7016a7be2862c9282
TS000 <tylersnell@me.com>
2020-10-07 11:34:48 -0600

    Class to Hooks. Refactored

--- src/containers/apply/ApplyForm.tsx
+++ src/containers/apply/ApplyForm.tsx
@@ -26,177 +26,156 @@ interface IApplyProps extends IApplication {
 }
 
 type ApplicationDispatch = ThunkDispatch<
-  IRootState,
-  undefined,
-  actions.SubmitFormAction | actions.UpdateApplicationAction
+IRootState,
+undefined,
+actions.SubmitFormAction | actions.UpdateApplicationAction
 >;
 
-const mapDispatchToProps = (dispatch: ApplicationDispatch) => {
-  return {
-    submitForm: () => {
-      dispatch(actions.submitForm());
-    },
-    toggleAcceptTos: () => {
-      dispatch(actions.toggleAcceptTos());
-    },
-    updateDescription: (value: IErrorableInput) => {
-      dispatch(actions.updateApplicationDescription(value));
-    },
-  };
-};
+const mapDispatchToProps = (dispatch: ApplicationDispatch) => ({
+  submitForm: () => {
+    dispatch(actions.submitForm());
+  },
+  toggleAcceptTos: () => {
+    dispatch(actions.toggleAcceptTos());
+  },
+  updateDescription: (value: IErrorableInput) => {
+    dispatch(actions.updateApplicationDescription(value));
+  },
+});
 
-const mapStateToProps = (state: IRootState) => {
-  return {
-    ...state.application,
-  };
-};
+const mapStateToProps = (state: IRootState) => ({
+  ...state.application,
+});
 
-class ApplyForm extends React.Component<IApplyProps> {
-  constructor(props: IApplyProps) {
-    super(props);
-  }
+const renderError = (props: any) => {
+  const assistanceTrailer = (
+    <span>
+      Need assistance? Create an issue through our <Link to="/support">Support page</Link>
+    </span>
+  );
 
-  public render() {
-    const {
-      inputs: { description, termsOfService },
-      ...props
-    } = this.props;
-    const applyClasses = classNames('vads-l-grid-container', 'vads-u-padding--4');
-
+  if (props.errorStatus) {
     return (
-      <div role="region" aria-labelledby={PAGE_HEADER_ID} className={applyClasses}>
-        <ApplyHeader />
-        <div className="vads-l-row">
-          <div
-            className={classNames(
-              'vads-l-col--12',
-              'medium-screen:vads-l-col--8',
-              'vads-u-padding-x--2p5',
-            )}
-          >
-            <form className="usa-form">
-              <h2>Application</h2>
-              <DeveloperInfo />
-              <SelectedApis />
-              {this.anyOAuthApisSelected() && <OAuthAppInfo />}
+      <AlertBox
+        status="error"
+        headline={'We encountered a server error while saving your form. Please try again later.'}
+        content={assistanceTrailer}
+      />
+    );
+  }
+  return null;
+};
 
-              <ErrorableTextArea
-                errorMessage={null}
-                label="Briefly describe how your organization will use VA APIs:"
-                onValueChange={props.updateDescription}
-                name="description"
-                field={description}
-              />
+const selectedApis = (props: any) => {
+  const apis = props.inputs.apis;
+  return Object.keys(apis).filter(apiName => apis[apiName]);
+};
 
-              <ErrorableCheckbox
-                checked={termsOfService}
-                label={
-                  <span>
+const anyOAuthApisSelected = (props: any) => {
+  const apiIdsByField = selectedApis(props).flatMap(
+    formField => APPLY_FIELDS_TO_URL_FRAGMENTS[formField],
+  );
+  return includesOauthAPI(apiIdsByField);
+};
+
+const anyApiSelected = (props: any) => {
+  const numSelected = selectedApis(props).length;
+  return numSelected > 0;
+};
+
+const allBioFieldsComplete = (props: any) => {
+  const bioFieldNames = ['email', 'firstName', 'lastName', 'organization'];
+  const incompleteFields = bioFieldNames.filter(fieldName => !props.inputs[fieldName].value);
+  return incompleteFields.length === 0;
+};
+
+const readyToSubmit = (props: any) => {
+  const { inputs: { oAuthApplicationType, oAuthRedirectURI, termsOfService }} = props;
+  let applicationTypeComplete = true;
+  let redirectURIComplete = true;
+  if (anyOAuthApisSelected(props)) {
+    applicationTypeComplete = oAuthApplicationType.value.length !== 0;
+    redirectURIComplete =
+      oAuthRedirectURI.value.length !== 0 && oAuthRedirectURI.validation === undefined;
+  }
+  return (
+    allBioFieldsComplete(props) &&
+    anyApiSelected(props) &&
+    termsOfService &&
+    applicationTypeComplete &&
+    redirectURIComplete
+  );
+};
+
+const ApplyForm = (props: IApplyProps) => {
+  const applyClasses = classNames('vads-l-grid-container', 'vads-u-padding--4');
+
+  return (
+    <div role="region" aria-labelledby={PAGE_HEADER_ID} className={applyClasses}>
+      <ApplyHeader />
+      <div className="vads-l-row">
+        <div
+          className={classNames(
+            'vads-l-col--12',
+            'medium-screen:vads-l-col--8',
+            'vads-u-padding-x--2p5',
+          )}
+        >
+          <form className="usa-form">
+            <h2>Application</h2>
+            <DeveloperInfo />
+            <SelectedApis />
+            {anyOAuthApisSelected(props) && <OAuthAppInfo />}
+
+            <ErrorableTextArea
+              errorMessage={null}
+              label="Briefly describe how your organization will use VA APIs:"
+              onValueChange={props.updateDescription}
+              name="description"
+              field={props.inputs.description}
+            />
+
+            <ErrorableCheckbox
+              checked={props.inputs.termsOfService}
+              label={
+                <span>
                     I agree to the <Link to="/terms-of-service">Terms of Service</Link>
-                  </span>
-                }
-                onValueChange={props.toggleAcceptTos}
-                required={true}
-              />
+                </span>
+              }
+              onValueChange={props.toggleAcceptTos}
+              required={true}
+            />
 
-              <ProgressButton
-                buttonText={props.sending ? 'Sending...' : 'Submit'}
-                disabled={!this.readyToSubmit() || props.sending}
-                onButtonClick={props.submitForm}
-                buttonClass="usa-button-primary"
-              />
-            </form>
-            {this.renderError()}
-          </div>
-          <div
-            className={classNames(
-              'vads-l-col--12',
-              'medium-screen:vads-l-col--4',
-              'vads-u-padding-x--2p5',
-            )}
-          >
-            <div className="feature">
-              <h3>Stay In Touch</h3>
-              <p>
+            <ProgressButton
+              buttonText={props.sending ? 'Sending...' : 'Submit'}
+              disabled={!readyToSubmit(props) || props.sending}
+              onButtonClick={props.submitForm}
+              buttonClass="usa-button-primary"
+            />
+          </form>
+          {renderError(props)}
+        </div>
+        <div
+          className={classNames(
+            'vads-l-col--12',
+            'medium-screen:vads-l-col--4',
+            'vads-u-padding-x--2p5',
+          )}
+        >
+          <div className="feature">
+            <h3>Stay In Touch</h3>
+            <p>
                 Want to get news and updates about VA API Program? Sign up to receive email updates.
-              </p>
-              <a
-                className="usa-button"
-                href="https://public.govdelivery.com/accounts/USVAOIT/subscriber/new?topic_id=USVAOIT_20"
-              >
+            </p>
+            <a className="usa-button" href="https://public.govdelivery.com/accounts/USVAOIT/subscriber/new?topic_id=USVAOIT_20">
                 Sign Up
-              </a>
-            </div>
+            </a>
           </div>
         </div>
       </div>
-    );
-  }
-
-  private renderError() {
-    const assistanceTrailer = (
-      <span>
-        Need assistance? Create an issue through our <Link to="/support">Support page</Link>
-      </span>
-    );
-
-    if (this.props.errorStatus) {
-      return (
-        <AlertBox
-          status="error"
-          headline={'We encountered a server error while saving your form. Please try again later.'}
-          content={assistanceTrailer}
-        />
-      );
-    }
-    return null;
-  }
-
-  private selectedApis() {
-    const apis = this.props.inputs.apis;
-    return Object.keys(apis).filter(apiName => apis[apiName]);
-  }
-
-  private anyOAuthApisSelected() {
-    const apiIdsByField = this.selectedApis().flatMap(
-      formField => APPLY_FIELDS_TO_URL_FRAGMENTS[formField],
-    );
-    return includesOauthAPI(apiIdsByField);
-  }
-
-  private anyApiSelected() {
-    const numSelected = this.selectedApis().length;
-    return numSelected > 0;
-  }
-
-  private allBioFieldsComplete() {
-    const bioFieldNames = ['email', 'firstName', 'lastName', 'organization'];
-    const incompleteFields = bioFieldNames.filter(fieldName => {
-      return !this.props.inputs[fieldName].value;
-    });
-    return incompleteFields.length === 0;
-  }
-
-  private readyToSubmit() {
-    const {
-      inputs: { oAuthApplicationType, oAuthRedirectURI, termsOfService },
-    } = this.props;
-    let applicationTypeComplete = true;
-    let redirectURIComplete = true;
-    if (this.anyOAuthApisSelected()) {
-      applicationTypeComplete = oAuthApplicationType.value.length !== 0;
-      redirectURIComplete =
-        oAuthRedirectURI.value.length !== 0 && oAuthRedirectURI.validation === undefined;
-    }
-    return (
-      this.allBioFieldsComplete() &&
-      this.anyApiSelected() &&
-      termsOfService &&
-      applicationTypeComplete &&
-      redirectURIComplete
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default connect(
   mapStateToProps,
