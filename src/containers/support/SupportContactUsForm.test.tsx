e9b9f3762ae6e869d76e47779341f57ca27fb021
johanna-adhoc <55552374+johanna-adhoc@users.noreply.github.com>
2020-10-09 14:16:20 -0400

    API-2536: Hooks Migration SupportContactUsForm Component (#448)
    * Migrated component SupportContactUsForm.
    
    * Fixed tslint issue and added interface def.
    
    * Added union types for action value.
    
    * Solved merge conflicts
    
    * Fixed test cases.
    
    Co-authored-by: Anthony Kopczyk <47796360+anthony-kopczyk@users.noreply.github.com>

--- src/containers/support/SupportContactUsForm.test.tsx
+++ src/containers/support/SupportContactUsForm.test.tsx
@@ -1,15 +1,15 @@
 import * as Sentry from '@sentry/browser';
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { mount } from 'enzyme';
 import 'jest';
 import { MockedRequest, rest, restContext } from 'msw';
 import { MockedResponse, ResponseComposition } from 'msw/lib/types/response';
 import { setupServer } from 'msw/node';
 import * as React from 'react';
+
 import { CONTACT_US_URL } from '../../types/constants';
 import SupportContactUsForm from './SupportContactUsForm';
-
-jest.mock('@sentry/browser');
-const mockedSentry = Sentry as jest.Mocked<typeof Sentry>;
 
 const server = setupServer(
   rest.post(
@@ -18,6 +18,11 @@ const server = setupServer(
       res(context.status(200), context.json({})),
   ),
 );
+
+/* eslint-disable  */
+// @ts-ignore
+const spyFetch = jest.spyOn(global, 'fetch');
+/* eslint-enable  */
 
 describe('SupportContactUsForm', () => {
   beforeAll(() => server.listen());
@@ -28,6 +33,98 @@ describe('SupportContactUsForm', () => {
     const onSuccessMock = jest.fn();
     const component = mount(<SupportContactUsForm onSuccess={onSuccessMock} />);
     expect(component.find('.usa-button-primary').hasClass('usa-button-disabled')).toBe(true);
+  });
+
+  it('Should send proper information on submission', async () => {
+    const handleSuccess = jest.fn();
+    const { getByLabelText, getByText } = render(
+      <SupportContactUsForm onSuccess={handleSuccess} />,
+    );
+
+    const firstNameInput = getByLabelText('First name(*Required)');
+    fireEvent.change(firstNameInput, { target: { value: 'john' } });
+
+    const lastNameInput = getByLabelText('Last name(*Required)');
+    fireEvent.change(lastNameInput, { target: { value: 'doe' } });
+
+    const description = getByLabelText(
+      'Please describe your question or issue in as much detail as you can provide. Steps to reproduce or any specific error messages are helpful if applicable.(*Required)',
+    );
+    fireEvent.change(description, { target: { value: 'this is a test' } });
+
+    const emailInput = getByLabelText('Email(*Required)');
+    fireEvent.change(emailInput, { target: { value: 'test@email.com' } });
+
+    const submitBtn = getByText('Submit');
+    await waitFor(() => expect(submitBtn).not.toHaveAttribute('disabled', ''));
+    fireEvent.click(submitBtn);
+
+    await waitFor(() => expect(spyFetch).toHaveBeenCalledTimes(1));
+
+    await waitFor(() =>
+      expect(spyFetch).toHaveBeenCalledWith({
+        _bodyInit:
+          '{"apis":[],"description":"this is a test","email":"test@email.com","firstName":"john","lastName":"doe","organization":""}',
+        _bodyText:
+          '{"apis":[],"description":"this is a test","email":"test@email.com","firstName":"john","lastName":"doe","organization":""}',
+        bodyUsed: true,
+        credentials: 'omit',
+        headers: { map: { accept: 'application/json', 'content-type': 'application/json' } },
+        method: 'POST',
+        mode: null,
+        referrer: null,
+        url: 'http://fake.va.gov/internal/developer-portal-backend/contact-us',
+      }),
+    );
+  });
+
+  it('logs error events in Sentry when fetch returns validation errors with status 400', async () => {
+    const captureException = jest.spyOn(Sentry, 'captureException');
+    const fromString = jest.spyOn(Sentry.Severity, 'fromString');
+    const scope = jest.spyOn(Sentry, 'withScope');
+
+    server.use(
+      rest.post(
+        CONTACT_US_URL,
+        (
+          req: MockedRequest,
+          res: ResponseComposition,
+          context: typeof restContext,
+        ): MockedResponse => res(context.status(400), context.json({ errors: ['unknown error'] })),
+      ),
+    );
+
+    const onSuccessMock = jest.fn();
+
+    const { getByLabelText, getByText } = render(
+      <SupportContactUsForm onSuccess={onSuccessMock} />,
+    );
+
+    const firstNameInput = getByLabelText('First name(*Required)');
+    fireEvent.change(firstNameInput, { target: { value: 'john' } });
+    const lastNameInput = getByLabelText('Last name(*Required)');
+    fireEvent.change(lastNameInput, { target: { value: 'doe' } });
+    const description = getByLabelText(
+      'Please describe your question or issue in as much detail as you can provide. Steps to reproduce or any specific error messages are helpful if applicable.(*Required)',
+    );
+    fireEvent.change(description, { target: { value: 'this is a test' } });
+    const emailInput = getByLabelText('Email(*Required)');
+    fireEvent.change(emailInput, { target: { value: 'hello@test.com' } });
+    const submitBtn = getByText('Submit');
+    await waitFor(() => expect(submitBtn).not.toHaveAttribute('disabled', ''));
+    fireEvent.click(submitBtn);
+
+    await waitFor(() => expect(Sentry.withScope).toHaveBeenCalled());
+
+    expect(Sentry.Severity.fromString).toHaveBeenCalledWith('warning');
+
+    expect(Sentry.captureException).toHaveBeenCalledWith(
+      Error('Contact Us Form validation errors: unknown error'),
+    );
+
+    captureException.mockRestore();
+    fromString.mockRestore();
+    scope.mockRestore();
   });
 
   it('should not be disabled when required fields are filled', done => {
@@ -77,75 +174,5 @@ describe('SupportContactUsForm', () => {
     const submitButton = component.find('.usa-button-primary');
     expect(submitButton.length).toEqual(1);
     expect(submitButton.hasClass('usa-button-disabled')).toBe(true);
-  });
-
-  it('should update state when field is filled', () => {
-    const onSuccessMock = jest.fn();
-    const component = mount(<SupportContactUsForm onSuccess={onSuccessMock} />);
-    const inputs = component.find('input[type="text"]');
-    expect(inputs.length).toEqual(4);
-
-    const firstNameInput = inputs.at(0);
-    expect(firstNameInput.length).toEqual(1);
-    firstNameInput.simulate('change', { target: { value: 'firstName' } });
-
-    const emailInput = inputs.at(2);
-    expect(emailInput.length).toEqual(1);
-    emailInput.simulate('change', { target: { value: 'email@gmail.com' } });
-
-    expect(component.state('firstName')).toMatchObject({ value: 'firstName' });
-    expect(component.state('email')).toMatchObject({ value: 'email@gmail.com' });
-  });
-
-  it('logs error events in Sentry when fetch returns validation errors with status 400', async () => {
-    server.use(
-      rest.post(
-        CONTACT_US_URL,
-        (
-          req: MockedRequest,
-          res: ResponseComposition,
-          context: typeof restContext,
-        ): MockedResponse =>
-          res(context.status(400), context.json({ errors: ['email must be valid email'] })),
-      ),
-    );
-
-    const onSuccessMock = jest.fn();
-    const formSubmissionSpy = jest.spyOn(SupportContactUsForm.prototype as any, 'formSubmission');
-    const component = mount(<SupportContactUsForm onSuccess={onSuccessMock} />);
-    component
-      .find(SupportContactUsForm)
-      .instance()
-      .setState({
-        apis: {
-          appeals: false,
-          benefits: true,
-          facilities: true,
-          health: false,
-          vaForms: false,
-          verification: false,
-        },
-        description: { value: 'help', dirty: true },
-        email: { value: 'test', dirty: true },
-        firstName: { value: 'Spongebob', dirty: true },
-        lastName: { value: 'Squarepants', dirty: true },
-        organization: { value: 'Krusty Krab', dirty: true },
-      });
-    component.update();
-
-    const form = component.find('Form');
-    form.find('.usa-button-primary').simulate('click');
-
-    expect(formSubmissionSpy).toHaveBeenCalled();
-    await formSubmissionSpy.mock.results[0].value;
-
-    const sentryCallback = mockedSentry.withScope.mock.calls[0][0];
-    const scope: any = {
-      setLevel: jest.fn(),
-    };
-    sentryCallback(scope);
-    expect(mockedSentry.captureException).toBeCalledWith(
-      Error('Contact Us Form validation errors: email must be valid email'),
-    );
   });
 });
--- src/containers/support/SupportContactUsForm.tsx
+++ src/containers/support/SupportContactUsForm.tsx
@@ -3,7 +3,9 @@ import ErrorableTextArea from '@department-of-veterans-affairs/formation-react/E
 import ErrorableTextInput from '@department-of-veterans-affairs/formation-react/ErrorableTextInput';
 import * as Sentry from '@sentry/browser';
 import classNames from 'classnames';
+import * as PropTypes from 'prop-types';
 import * as React from 'react';
+
 import { getEnabledApiCategories } from '../../apiDefs/env';
 import { getApiDefinitions } from '../../apiDefs/query';
 import Form from '../../components/Form';
@@ -12,6 +14,15 @@ import { CONTACT_US_URL } from '../../types/constants';
 import { validateEmail, validatePresence } from '../../utils/validators';
 
 import './SupportContactUsForm.scss';
+
+/**
+ * PROP TYPES AND TYPEDEF
+ */
+const SupportContactUsFormPropTypes = {
+  onSuccess: PropTypes.func.isRequired,
+};
+
+type SupportContactUsFormProps = PropTypes.InferProps<typeof SupportContactUsFormPropTypes>;
 
 interface SupportContactUsFormState {
   apis: { [x: string]: boolean };
@@ -22,177 +33,79 @@ interface SupportContactUsFormState {
   organization: IErrorableInput;
 }
 
-interface SupportContactUsFormProps {
-  onSuccess: () => void;
+interface FormData {
+  apis: string[];
+  description: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  organization: string;
 }
 
-/* eslint-disable @typescript-eslint/indent */
-export default class SupportContactUsForm extends React.Component<
-  SupportContactUsFormProps,
-  SupportContactUsFormState
-> {
-  /* eslint-enable @typescript-eslint/indent */
-  public constructor(props: SupportContactUsFormProps) {
-    super(props);
-    this.state = {
-      apis: SupportContactUsForm.initialApiState,
-      description: SupportContactUsForm.defaultErrorableField,
-      email: SupportContactUsForm.defaultErrorableField,
-      firstName: SupportContactUsForm.defaultErrorableField,
-      lastName: SupportContactUsForm.defaultErrorableField,
-      organization: SupportContactUsForm.defaultErrorableField,
-    };
+/**
+ * STATE REDUCER
+ */
+const initialApiState = () =>
+  getEnabledApiCategories().reduce((accumulator, api) => {
+    accumulator[api] = false;
+    return accumulator;
+  }, {});
 
-    this.formSubmission = this.formSubmission.bind(this) as () => Promise<void>;
-    this.toggleApis = this.toggleApis.bind(this) as () => void;
+const defaultErrorableField = (): IErrorableInput => ({
+  dirty: false,
+  value: '',
+});
+
+const initialFormState = {
+  apis: initialApiState(),
+  description: defaultErrorableField(),
+  email: defaultErrorableField(),
+  firstName: defaultErrorableField(),
+  lastName: defaultErrorableField(),
+  organization: defaultErrorableField(),
+};
+
+type actionValue = { [x: string]: boolean } | IErrorableInput;
+
+const reducer = (
+  state: SupportContactUsFormState,
+  action: { type: string; value: actionValue },
+): SupportContactUsFormState => {
+  switch (action.type) {
+    case 'SET_APIS':
+      return { ...state, apis: action.value as { [x: string]: boolean } };
+    case 'SET_DESCRIPTION':
+      return { ...state, description: action.value as IErrorableInput };
+    case 'SET_EMAIL':
+      return { ...state, email: action.value as IErrorableInput };
+    case 'SET_FIRST_NAME':
+      return { ...state, firstName: action.value as IErrorableInput };
+    case 'SET_LAST_NAME':
+      return { ...state, lastName: action.value as IErrorableInput };
+    case 'SET_ORGANIZATION':
+      return { ...state, organization: action.value as IErrorableInput };
+    default:
+      return state;
   }
+};
 
-  public render(): JSX.Element {
-    const legendDescClasses = classNames('vads-u-font-size--md', 'vads-u-font-weight--normal');
-    const textFieldClasses = (paddingDirection: string): string =>
-      classNames(
-        'vads-l-col--12',
-        'small-screen:vads-l-col--6',
-        `small-screen:vads-u-padding-${paddingDirection}--2`,
-      );
+const apiOptions = (): Array<{ label: string; value: string }> => {
+  const apiDefs = getApiDefinitions();
+  return getEnabledApiCategories().map(api => ({
+    label: apiDefs[api].name,
+    value: api,
+  }));
+};
 
-    return (
-      <Form
-        /* eslint-disable @typescript-eslint/unbound-method */
-        onSubmit={this.formSubmission}
-        /* eslint-enable @typescript-eslint/unbound-method */
-        onSuccess={this.props.onSuccess}
-        disabled={!this.isFormValid}
-        className={classNames('va-api-contact-us-form', 'vads-u-margin-y--2')}
-      >
-        <fieldset>
-          <legend className="vads-u-font-size--lg">
-            Contact Us
-            <p className={legendDescClasses}>
-              Have a question? Use the form below to send us an email and we&apos;ll do the best to
-              answer your question and get you headed in the right direction.
-            </p>
-          </legend>
+/**
+ * COMPONENT
+ */
+const SupportContactUsForm = (props: SupportContactUsFormProps): JSX.Element => {
+  const [formState, setFormState] = React.useReducer(reducer, initialFormState);
 
-          <div className={classNames('vads-l-grid-container', 'vads-u-padding-x--0')}>
-            <div className="vads-l-row">
-              <div className={textFieldClasses('right')}>
-                <ErrorableTextInput
-                  errorMessage={this.state.firstName.validation}
-                  label="First name"
-                  field={this.state.firstName}
-                  onValueChange={(field: IErrorableInput) =>
-                    this.setState({ firstName: validatePresence(field, 'First Name') })
-                  }
-                  required
-                />
-              </div>
-              <div className={textFieldClasses('left')}>
-                <ErrorableTextInput
-                  errorMessage={this.state.lastName.validation}
-                  label="Last name"
-                  name="lastName"
-                  field={this.state.lastName}
-                  onValueChange={(field: IErrorableInput) =>
-                    this.setState({ lastName: validatePresence(field, 'Last Name') })
-                  }
-                  required
-                />
-              </div>
-            </div>
-            <div className="vads-l-row">
-              <div className={textFieldClasses('right')}>
-                <ErrorableTextInput
-                  errorMessage={this.state.email.validation}
-                  label="Email"
-                  name="email"
-                  field={this.state.email}
-                  onValueChange={(field: IErrorableInput) =>
-                    this.setState({ email: validateEmail(field) })
-                  }
-                  required
-                />
-              </div>
-              <div className={textFieldClasses('left')}>
-                <ErrorableTextInput
-                  errorMessage={null}
-                  label="Organization"
-                  name="organization"
-                  field={this.state.organization}
-                  onValueChange={(field: IErrorableInput) => this.setState({ organization: field })}
-                  required={false}
-                />
-              </div>
-            </div>
-          </div>
-
-          <ErrorableCheckboxGroup
-            additionalFieldsetClass="vads-u-margin-top--4"
-            additionalLegendClass={legendDescClasses}
-            label="If applicable, please select any of the APIs pertaining to your issue."
-            /* eslint-disable @typescript-eslint/unbound-method */
-            onValueChange={this.toggleApis}
-            /* eslint-enable @typescript-eslint/unbound-method */
-            id="default"
-            required={false}
-            options={SupportContactUsForm.apiOptions}
-            values={{ key: 'value' }}
-          />
-
-          <ErrorableTextArea
-            errorMessage={this.state.description.validation}
-            label="Please describe your question or issue in as much detail as you can provide. Steps to reproduce or any specific error messages are helpful if applicable."
-            onValueChange={(field: IErrorableInput) =>
-              this.setState({ description: validatePresence(field, 'Description') })
-            }
-            name="description"
-            field={this.state.description}
-            required
-          />
-        </fieldset>
-      </Form>
-    );
-  }
-
-  private static get apiOptions(): Array<{
-    label: string;
-    value: string;
-  }> {
-    const apiDefs = getApiDefinitions();
-    return getEnabledApiCategories().map(api => ({
-      label: apiDefs[api].name,
-      value: api,
-    }));
-  }
-
-  private static get initialApiState() {
-    return getEnabledApiCategories().reduce((accumulator, api) => {
-      accumulator[api] = false;
-      return accumulator;
-    }, {});
-  }
-
-  private static get defaultErrorableField(): IErrorableInput {
-    return {
-      dirty: false,
-      value: '',
-    };
-  }
-
-  private get processedData(): any {
-    return {
-      apis: Object.keys(this.state.apis).filter(k => this.state.apis[k]),
-      description: this.state.description.value,
-      email: this.state.email.value,
-      firstName: this.state.firstName.value,
-      lastName: this.state.lastName.value,
-      organization: this.state.organization.value,
-    };
-  }
-
-  private get isFormValid(): boolean {
+  const isFormValid = (): boolean => {
     const validateField = (field: IErrorableInput): boolean => !!field.value && !field.validation;
-    const { description, email, firstName, lastName } = this.state;
+    const { description, email, firstName, lastName } = formState;
 
     return (
       validateField(firstName) &&
@@ -200,18 +113,20 @@ export default class SupportContactUsForm extends React.Component<
       validateField(email) &&
       validateField(description)
     );
-  }
+  };
 
-  private toggleApis(input: IErrorableInput, checked: boolean) {
-    const name = input.value;
-    const apis = this.state.apis;
-    apis[name] = checked;
-    this.setState({ apis });
-  }
+  const processedData = (): FormData => ({
+    apis: Object.keys(formState.apis).filter(k => formState.apis[k]),
+    description: formState.description.value,
+    email: formState.email.value,
+    firstName: formState.firstName.value,
+    lastName: formState.lastName.value,
+    organization: formState.organization.value,
+  });
 
-  private async formSubmission() {
+  const formSubmission = () => {
     const request = new Request(CONTACT_US_URL, {
-      body: JSON.stringify(this.processedData),
+      body: JSON.stringify(processedData()),
       headers: {
         accept: 'application/json',
         'content-type': 'application/json',
@@ -239,5 +154,128 @@ export default class SupportContactUsForm extends React.Component<
           Sentry.captureException(error);
         });
       });
-  }
-}
+  };
+
+  const toggleApis = (input: IErrorableInput, checked: boolean) => {
+    const name = input.value;
+    const apis = formState.apis;
+    apis[name] = checked;
+    setFormState({ type: 'SET_APIS', value: apis });
+  };
+
+  const legendDescClasses = classNames('vads-u-font-size--md', 'vads-u-font-weight--normal');
+  const textFieldClasses = (paddingDirection: string): string =>
+    classNames(
+      'vads-l-col--12',
+      'small-screen:vads-l-col--6',
+      `small-screen:vads-u-padding-${paddingDirection}--2`,
+    );
+
+  /**
+   * RENDER
+   */
+  return (
+    <Form
+      onSubmit={formSubmission}
+      onSuccess={props.onSuccess}
+      disabled={!isFormValid()}
+      className={classNames('va-api-contact-us-form', 'vads-u-margin-y--2')}
+    >
+      <fieldset>
+        <legend className="vads-u-font-size--lg">
+          Contact Us
+          <p className={legendDescClasses}>
+            Have a question? Use the form below to send us an email and we&#39;ll do the best to
+            answer your question and get you headed in the right direction.
+          </p>
+        </legend>
+
+        <div className={classNames('vads-l-grid-container', 'vads-u-padding-x--0')}>
+          <div className="vads-l-row">
+            <div className={textFieldClasses('right')}>
+              <ErrorableTextInput
+                errorMessage={formState.firstName.validation}
+                label="First name"
+                field={formState.firstName}
+                onValueChange={(field: IErrorableInput) =>
+                  setFormState({
+                    type: 'SET_FIRST_NAME',
+                    value: validatePresence(field, 'First Name'),
+                  })
+                }
+                required
+              />
+            </div>
+            <div className={textFieldClasses('left')}>
+              <ErrorableTextInput
+                errorMessage={formState.lastName.validation}
+                label="Last name"
+                name="lastName"
+                field={formState.lastName}
+                onValueChange={(field: IErrorableInput) =>
+                  setFormState({
+                    type: 'SET_LAST_NAME',
+                    value: validatePresence(field, 'Last Name'),
+                  })
+                }
+                required
+              />
+            </div>
+          </div>
+          <div className="vads-l-row">
+            <div className={textFieldClasses('right')}>
+              <ErrorableTextInput
+                errorMessage={formState.email.validation}
+                label="Email"
+                name="email"
+                field={formState.email}
+                onValueChange={(field: IErrorableInput) =>
+                  setFormState({ type: 'SET_EMAIL', value: validateEmail(field) })
+                }
+                required
+              />
+            </div>
+            <div className={textFieldClasses('left')}>
+              <ErrorableTextInput
+                errorMessage={null}
+                label="Organization"
+                name="organization"
+                field={formState.organization}
+                onValueChange={(field: IErrorableInput) =>
+                  setFormState({ type: 'SET_ORGANIZATION', value: field })
+                }
+                required={false}
+              />
+            </div>
+          </div>
+        </div>
+
+        <ErrorableCheckboxGroup
+          additionalFieldsetClass="vads-u-margin-top--4"
+          additionalLegendClass={legendDescClasses}
+          label="If applicable, please select any of the APIs pertaining to your issue."
+          onValueChange={toggleApis}
+          id="default"
+          required={false}
+          options={apiOptions()}
+          values={{ key: 'value' }}
+        />
+
+        <ErrorableTextArea
+          errorMessage={formState.description.validation}
+          label="Please describe your question or issue in as much detail as you can provide. Steps to reproduce or any specific error messages are helpful if applicable."
+          onValueChange={(field: IErrorableInput) =>
+            setFormState({ type: 'SET_DESCRIPTION', value: validatePresence(field, 'Description') })
+          }
+          name="description"
+          field={formState.description}
+          required
+        />
+      </fieldset>
+    </Form>
+  );
+};
+
+SupportContactUsForm.propTypes = SupportContactUsFormPropTypes;
+
+export default SupportContactUsForm;
