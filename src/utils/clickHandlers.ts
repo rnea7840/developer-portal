27877d5c0500101f754ac154398db5b2972a63fc
Giles Wells <giles.wells@oddball.io>
2020-10-15 11:03:54 -0400

    Hotfix/secondary skip nav timing issue (#490)
    * Add setTimeout to fix skip nav focus bug
    
    * Transposed ?

--- src/utils/clickHandlers.ts
+++ src/utils/clickHandlers.ts
@@ -4,6 +4,8 @@ export const onHashAnchorClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
   // This provides for focus to be applied when clicking a /hash#link
   const id: string = e.currentTarget.href.split('#')?.[1];
   if (id) {
-    document.getElementById(id)?.focus();
+    setTimeout(() => {
+      document.getElementById(id)?.focus();
+    }, 0);
   }
 };
