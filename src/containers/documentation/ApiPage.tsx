bc3057fabc96bf294b6516a4890ed2ecbe86a1da
Anthony Kopczyk <47796360+anthony-kopczyk@users.noreply.github.com>
2020-10-16 08:15:20 -0400

    API-2650: Use route hooks for ApiPage (#484)
    * Use route hooks for ApiPage
    
    Co-authored-by: Tyler Snell <tylersnell@me.com>

--- src/containers/documentation/ApiPage.tsx
+++ src/containers/documentation/ApiPage.tsx
@@ -1,6 +1,6 @@
-import classNames from 'classnames';
 import * as React from 'react';
-import { RouteComponentProps } from 'react-router';
+import { useLocation, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 import { isApiDeactivated, isApiDeprecated } from '../../apiDefs/deprecated';
 import { lookupApiByFragment, lookupApiCategory } from '../../apiDefs/query';
 import { APIDescription } from '../../apiDefs/schema';
@@ -37,8 +37,9 @@ const getApi = (apiName?: string): APIDescription | null => {
   return lookupApiByFragment(apiName);
 };
 
-const ApiPage = (props: RouteComponentProps<APINameParam>): JSX.Element => {
-  const { params } = props.match;
+const ApiPage = (): JSX.Element => {
+  const location = useLocation();
+  const params = useParams<APINameParam>();
 
   const api = getApi(params.apiName);
   if (api === null) {
@@ -56,7 +57,7 @@ const ApiPage = (props: RouteComponentProps<APINameParam>): JSX.Element => {
           <ApiDocumentation
             apiDefinition={api}
             categoryKey={params.apiCategoryKey}
-            location={props.location}
+            location={location}
           />
         )}
       </div>
