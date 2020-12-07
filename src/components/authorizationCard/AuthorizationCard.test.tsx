cd023fc572e707f7d90bd3f00ee7c1c702599211
Anthony Kopczyk <47796360+anthony-kopczyk@users.noreply.github.com>
2020-12-07 12:34:08 -0500

    Add initial tests for AuthorizationCard (#567)
    * Add tests for AuthorizationCard

---
+++ src/components/authorizationCard/AuthorizationCard.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import 'jest';
+import * as React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthorizationCard } from '..';
+
+describe('Authorization Card', () => {
+  it('should render default link', () => {
+    render(
+      <MemoryRouter>
+        <AuthorizationCard />
+      </MemoryRouter>,
+    );
+    const cardLink = screen.getByRole('link');
+    expect(cardLink).toHaveAttribute('href', '/explore/authorization');
+  });
+
+  it('should render link based on passed categoryKey', () => {
+    render(
+      <MemoryRouter>
+        <AuthorizationCard categoryKey="test" />
+      </MemoryRouter>,
+    );
+    const cardLink = screen.getByRole('link');
+    expect(cardLink).toHaveAttribute('href', '/explore/test/docs/authorization');
+  });
+
+  it('should render correct text content', () => {
+    render(
+      <MemoryRouter>
+        <AuthorizationCard />
+      </MemoryRouter>,
+    );
+    const elementContainingContent = screen.getByText(
+      'Use the OpenID Connect standard to allow Veterans to authorize third-party application to access data on their behalf.',
+    );
+    expect(elementContainingContent).toBeDefined();
+  });
+});
