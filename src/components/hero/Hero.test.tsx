8187e3f671b0b60e594230553a1c06f82e9f3f36
Tyler Snell <tylersnell@me.com>
2020-12-16 13:50:00 -0700

    API-3757: Test for Hero Component (#573)
    * Checks for image source
    
    * fixed extra space and used screen
    
    * added link role to api link
    
    * added api key url check
    
    * removed role for link
    
    * changed role to text for the free test of text
    
    Co-authored-by: johanna-adhoc <55552374+johanna-adhoc@users.noreply.github.com>

---
+++ src/components/hero/Hero.test.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import 'jest';
+import '@testing-library/jest-dom/extend-expect';
+import {  render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import * as logoImage from '../../__mocks__/fakeImage';
+import { Hero } from './Hero';
+
+describe('Hero', () => {
+  it('checks image source', () => {
+    render(
+      <Router>
+        <Hero />
+      </Router>
+    );
+
+    const heroImage = screen.getByRole('presentation');
+    expect(heroImage).toBeInTheDocument();
+    expect(heroImage).toHaveAttribute('src', (logoImage.default));
+  });
+
+  it('checks api key url', () => {
+    render(
+      <Router>
+        <Hero />
+      </Router>
+    );
+
+    const apiLink = screen.getByText('Request an API Key');
+    expect(apiLink).toBeInTheDocument();
+    expect(apiLink).toHaveAttribute('href', ('/apply'));
+  });
+
+  it('checks h1 text', () => {
+    render(
+      <Router>
+        <Hero />
+      </Router>
+    );
+
+    const desiredText = 'A Veteran-centered API platform for securely accessing VA data.';
+    const heroText = screen.getByText(desiredText);
+    expect(heroText).toBeInTheDocument();
+    expect(heroText).toHaveClass('vads-u-color--white',
+      'vads-u-font-size--h2',
+      'small-desktop-screen:vads-u-font-size--h1');
+  });
+});
