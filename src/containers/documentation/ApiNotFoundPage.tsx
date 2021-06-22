ea9f3481b27d08a0a894c9fbf8c65e98c849a8d5
Giles Wells <giles@mediagearhead.com>
2021-06-22 10:46:56 -0400

    Fix open redirect in ApiNotFoundPage.tsx

--- src/containers/documentation/ApiNotFoundPage.tsx
+++ src/containers/documentation/ApiNotFoundPage.tsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 
 import { isApiDeactivated } from '../../apiDefs/deprecated';
-import { lookupApiCategory } from '../../apiDefs/query';
+import { lookupApiByFragment, lookupApiCategory } from '../../apiDefs/query';
 import { APIDescription } from '../../apiDefs/schema';
 import { PageHeader } from '../../components';
 import { APINameParam } from '../../types';
@@ -14,6 +14,8 @@ import { PAGE_HEADER_ID } from '../../types/constants';
 const ApiNotFoundPage = (): JSX.Element => {
   const { apiCategoryKey } = useParams<APINameParam>();
   const category = lookupApiCategory(apiCategoryKey);
+  // You can't get to this page without a valid API so this ?? is just for TypeScript's benefit
+  const api = lookupApiByFragment(apiCategoryKey) ?? { urlFragment: '' };
 
   return (
     <div role="region" aria-labelledby={PAGE_HEADER_ID}>
@@ -29,7 +31,7 @@ const ApiNotFoundPage = (): JSX.Element => {
           .filter((item: APIDescription) => !isApiDeactivated(item))
           .map((item: APIDescription) => (
             <li key={item.urlFragment}>
-              <Link to={`/explore/${apiCategoryKey}/docs/${item.urlFragment}`}>{item.name}</Link>
+              <Link to={`/explore/${api.urlFragment}/docs/${item.urlFragment}`}>{item.name}</Link>
             </li>
           ))}
       </ul>
