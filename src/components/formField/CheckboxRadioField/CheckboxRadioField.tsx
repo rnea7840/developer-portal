742c629534dc9ee17bb9ba73a20406a3a05cd59d
Mike Lumetta <mike.lumetta@adhocteam.us>
2021-08-24 11:09:31 -0400

    Set focus on first input with an error after validation for each step in the prod access form (#840)

--- src/components/formField/CheckboxRadioField/CheckboxRadioField.tsx
+++ src/components/formField/CheckboxRadioField/CheckboxRadioField.tsx
@@ -1,4 +1,4 @@
-import { Field } from 'formik';
+import { Field, getIn, useFormikContext } from 'formik';
 import React, { FC, ReactNode } from 'react';
 import toHtmlId from '../../../toHtmlId';
 
@@ -21,16 +21,26 @@ const CheckboxRadioField: FC<CheckboxRadioFieldProps> = ({
   value,
   ...props
 }) => {
+  const { errors, touched } = useFormikContext();
   const radioClass =
     type === 'radio' ? 'vads-u-margin--0 vads-u-padding-y--1 vads-u-padding-x--1p5' : '';
 
   const idReadyName = toHtmlId(name);
   const idReadyValue = toHtmlId(value ?? '');
   const fieldId = `${idReadyName}FormField${idReadyValue}`;
+  const shouldDisplayErrors = !!getIn(errors, name) && !!getIn(touched, name);
 
   return (
     <div className={className}>
-      <Field id={fieldId} name={name} type={type} value={value} innerRef={innerRef} {...props} />
+      <Field
+        id={fieldId}
+        name={name}
+        type={type}
+        value={value}
+        innerRef={innerRef}
+        aria-invalid={shouldDisplayErrors}
+        {...props}
+      />
       <label htmlFor={fieldId} className={radioClass}>
         {label}
       </label>
--- src/containers/consumerOnboarding/ProductionAccess.tsx
+++ src/containers/consumerOnboarding/ProductionAccess.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-dynamic-delete, id-length, max-lines */
-
 import React, { FC, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Formik, Form, FormikHelpers } from 'formik';
@@ -160,6 +159,16 @@ const renderStepContent = (step: number): JSX.Element => {
   }
 };
 
+const handleSubmitButtonClick = (): void => {
+  setTimeout(() => {
+    const errorElements = document.querySelectorAll<HTMLElement>('[aria-invalid=true]');
+
+    if (errorElements.length > 0) {
+      errorElements[0].focus();
+    }
+  }, 0);
+};
+
 const ProductionAccess: FC = () => {
   const [submissionError, setSubmissionError] = useState(false);
   const [activeStep, setActiveStep] = useState(0);
@@ -207,13 +216,6 @@ const ProductionAccess: FC = () => {
     }
   };
   const handleSubmit = async (values: Values, actions: FormikHelpers<Values>): Promise<void> => {
-    setTimeout(() => {
-      const errorElements = document.querySelectorAll<HTMLElement>('[aria-invalid=true]');
-
-      if (errorElements.length > 0) {
-        errorElements[0].focus();
-      }
-    }, 0);
     if (isLastStep) {
       setSubmissionError(false);
       delete values.is508Compliant;
@@ -340,11 +342,16 @@ const ProductionAccess: FC = () => {
                   <button
                     type="submit"
                     className="usa-button-primary va-button-primary vads-u-width--auto"
+                    onClick={handleSubmitButtonClick}
                   >
                     Submit your application
                   </button>
                 ) : (
-                  <button type="submit" className="usa-button vads-u-width--auto">
+                  <button
+                    type="submit"
+                    className="usa-button vads-u-width--auto"
+                    onClick={handleSubmitButtonClick}
+                  >
                     Continue <FontAwesomeIcon icon={faAngleDoubleRight} />
                   </button>
                 )}
