7b52db8f9bec682f6711344bd220ad1ffcbdab8a
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2021-09-13 20:31:09 -0600

    Changes context for 6910, 6905, 6902

--- src/types/get-video-id.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'get-video-id' {
-  interface VideoInfo {
-    id: string;
-    service: string;
-  }
-
-  const getVideoId: (url: string) => VideoInfo;
-  export default getVideoId;
-}
