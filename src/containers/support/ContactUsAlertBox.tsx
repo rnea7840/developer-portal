4c7ed56250d6e2df323eeb201bedab98c691b8c3
Calvin Gonzalez <81174472+Cgfor3@users.noreply.github.com>
2021-10-19 11:15:46 -0400

    [API-9661] Veteran Redirect Banner Update (#896)
    - Update banner copy
    - Add new redirect link to VSO and submit a question form
    - Update visual regression tests

--- src/containers/support/ContactUsAlertBox.tsx
+++ src/containers/support/ContactUsAlertBox.tsx
@@ -4,8 +4,9 @@ import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox
 export const ContactUsAlertBox = (): JSX.Element => (
   <AlertBox status="info" className="vads-u-margin-bottom--2 vads-u-padding-y--1">
     <p>
-      Are you a Veteran? Contact your local VSO or visit <a href="https://www.va.gov">VA.gov</a> to
-      get the help you need.
+      Are you a Veteran or looking for information on behalf of a Veteran?{' '}
+      <a href="https://www.va.gov/vso">Contact your local VSO</a> or{' '}
+      <a href="https://iris.custhelp.va.gov/app/ask">submit a question</a> to get the help you need.
     </p>
   </AlertBox>
 );
