a6872ccddfa35fd232c41dadeee70dda782f482f
Giles Wells <giles.wells@oddball.io>
2021-12-01 11:21:59 -0500

    Fix width issue on Safari (#914)

--- src/containers/documentation/AuthorizationDocs.tsx
+++ src/containers/documentation/AuthorizationDocs.tsx
@@ -12,27 +12,35 @@ const AuthorizationDocs = (): JSX.Element => {
 
   return (
     <div className="va-api-authorization-docs">
-      {api && <Redirect to={newAuthDocLOcation} /> }
+      {api && <Redirect to={newAuthDocLOcation} />}
       <Helmet>
         <title>Authorization</title>
       </Helmet>
-      <PageHeader
-        header="Authorization"
-        description="Explore available OAuth 2.0 grant flows"
-      />
+      <PageHeader header="Authorization" description="Explore available OAuth 2.0 grant flows" />
       <div className={defaultFlexContainer()}>
-        <div>
+        <div className="vads-l-col--12">
           <p>
-            What you use depends on your application type and API. <Link to="/explore">Learn more about our APIs.</Link>
+            What you use depends on your application type and API.{' '}
+            <Link to="/explore">Learn more about our APIs.</Link>
           </p>
           <p>
-            <Link to="/onboarding">Read the consumer onboarding guide for getting production access.</Link>
+            <Link to="/onboarding">
+              Read the consumer onboarding guide for getting production access.
+            </Link>
           </p>
         </div>
-        <CardLink name="Authorization Code Flow" url="/explore/authorization/docs/authorization-code" callToAction="View the Authorization Code Flow docs">
+        <CardLink
+          name="Authorization Code Flow"
+          url="/explore/authorization/docs/authorization-code"
+          callToAction="View the Authorization Code Flow docs"
+        >
           Grants an access token on behalf of a user.
         </CardLink>
-        <CardLink name="Client Credentials Grant" url="/explore/authorization/docs/client-credentials" callToAction="View the Client Credentials Grant docs">
+        <CardLink
+          name="Client Credentials Grant"
+          url="/explore/authorization/docs/client-credentials"
+          callToAction="View the Client Credentials Grant docs"
+        >
           Grants an access token on behalf of a system.
         </CardLink>
       </div>
