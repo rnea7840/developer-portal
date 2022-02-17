2b241fb6b4a8ca6f03dd88cb88f436916b90de7c
Lee DeBoom <lee.deboom@oddball.io>
2022-02-17 11:26:21 -0600

    API-11956

--- src/containers/documentation/ApiDocumentation.tsx
+++ src/containers/documentation/ApiDocumentation.tsx
@@ -78,8 +78,8 @@ const ApiDocumentation = (props: ApiDocumentationProps): JSX.Element => {
    */
   return (
     <Flag name={[FLAG_HOSTED_APIS, apiDefinition.urlFragment]}>
-      {(apiDefinition.urlFragment === 'verification' ||
-        apiDefinition.urlFragment === 'health') && (
+      {(apiDefinition.urlFragment === 'veteran_verification' ||
+        apiDefinition.urlFragment === 'fhir') && (
         <div role="region" aria-labelledby="oauth-info-heading" className="api-docs-oauth-link">
           <h2 id="oauth-info-heading" className="usa-alert-heading">
             Authentication and Authorization
