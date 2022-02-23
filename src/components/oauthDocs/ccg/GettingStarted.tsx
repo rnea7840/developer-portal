b5bffcaf794d928909322e8e04ecfdb11424da1d
Giles Wells <giles@mediagearhead.com>
2022-02-23 15:16:24 -0500

    Fix small but impactful typos in mailto: links

--- src/components/oauthDocs/ccg/GettingStarted.tsx
+++ src/components/oauthDocs/ccg/GettingStarted.tsx
@@ -8,8 +8,9 @@ const GettingStarted = (): JSX.Element => (
   <>
     <SectionHeaderWrapper heading="Getting Started" id="getting-started" />
     <p>
-      After you <Link to="/apply">request sandbox access</Link>, you will need to generate your RSA key pair and convert
-      the public key into JWK format. What you generate will look similar to this:
+      After you <Link to="/apply">request sandbox access</Link>, you will need to generate your RSA
+      key pair and convert the public key into JWK format. What you generate will look similar to
+      this:
     </p>
     <CodeWrapper>
       <ReactMarkdown rehypePlugins={[highlight]}>
@@ -21,12 +22,12 @@ const GettingStarted = (): JSX.Element => (
   "alg": "RS256",
   "use": "sig"
 }
-`         }
+`}
       </ReactMarkdown>
     </CodeWrapper>
     <p>
-      Then, send this key to our support team at <a href="mailto:api.va.gov">api@va.gov</a>,
-      and we will send you a client ID.
+      Then, send this key to our support team at <a href="mailto:api@va.gov">api@va.gov</a>, and we
+      will send you a client ID.
     </p>
   </>
 );
