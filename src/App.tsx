08f1e59bc3652f3cddf5802f7b9d8f36c08a7b36
Andres Rivera <andres.rivera.toro@gmail.com>
2022-11-02 10:11:56 -0400

    chore: update polyfill import path

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { Route, Router } from 'react-router-dom';
 import { useDispatch, connect } from 'react-redux';
-import { applyPolyfills, defineCustomElements } from 'web-components/loader';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@department-of-veterans-affairs/component-library';
 import { LPB_PROVIDERS_URL } from './types/constants';
 import { setApiLoadingError, SetAPIs, setApis } from './actions';
 import { APICategories } from './apiDefs/schema';
