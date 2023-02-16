852e7ca6d3daccf286e80cac6c2d185095f49e63
Calvin Gonzalez <81174472+Cgfor3@users.noreply.github.com>
2023-02-16 13:57:33 -0500

    [API-23395] Update CCG docs disclaimer (#1176)
    * [API-23395] Update CCG docs disclaimer
    - Add disclaimer to "Good To Know" section and in "aud" table row
    
    * Updated visual snapshots via GitHub Actions job
    
    ---------
    
    Co-authored-by: GitHub Action Runner <giles.wells@oddball.io>

--- src/components/oauthDocs/ccg/GoodToKnow.tsx
+++ src/components/oauthDocs/ccg/GoodToKnow.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
-import { CONSUMER_PROD_PATH } from '../../../types/constants/paths';
+import { CONSUMER_PROD_PATH, SUPPORT_CONTACT_PATH } from '../../../types/constants/paths';
 
 const GoodToKnow: FC = () => (
   <>
@@ -9,6 +9,10 @@ const GoodToKnow: FC = () => (
       <li>
         The access credentials we supply are for the sandbox environment only and will not work in
         the production environment.
+      </li>
+      <li>
+        To get production access, you must either work for VA or have specific VA agreements in
+        place. If you have questions, <Link to={SUPPORT_CONTACT_PATH}>contact us</Link>.
       </li>
       <li>
         This page provides examples that show authorization server URLs in the sandbox environment,
