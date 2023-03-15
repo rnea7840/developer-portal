9fbf12ef513bf702b9bc1d60829e36b8042f9145
Ian McGonigle <ian.mcgonigle@oddball.io>
2023-03-15 16:16:30 -0600

    Increase puppeteer timeout

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -18,7 +18,7 @@ if (!process.env.TEST_HOST) {
     command: `${envVars.join(' ')} npm run start`,
     protocol: 'http',
     port: 4444,
-    launchTimeout: 120000,
+    launchTimeout: 300000,
   };
 }
 
