b0d36a7bb19e4c54e39eecf6fe8d7ffbc42db957
Giles Wells <giles@mediagearhead.com>
2023-03-20 16:57:08 -0400

    Typescript and timeout fixes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,6 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  env: {
-    REACT_APP_VETSGOV_SWAGGER_API: 'http://localhost:3001',
-  },
   blockHosts: [
     'dap.digitalgov.gov',
     'search.usa.gov',
@@ -12,20 +9,17 @@ export default defineConfig({
     'googleads.g.doubleclick.net',
     'www.youtube.com',
   ],
-  trashAssetsBeforeRuns: false,
-  updateSnapshots: false,
-  video: false,
   e2e: {
+    baseUrl: 'http://localhost:3001',
+    excludeSpecPattern: ['accessibility.spec.js', 'smoketest.spec.js', 'visual.spec.js'],
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    baseUrl: 'http://localhost:3001',
-    excludeSpecPattern: [
-      'accessibility.spec.js',
-      'smoketest.spec.js',
-      'visual.spec.js',
-    ],
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
+    setupNodeEvents: (on, config) => require('./cypress/plugins/index.js')(on, config),
   },
-})
+  env: {
+    REACT_APP_VETSGOV_SWAGGER_API: 'http://localhost:3001',
+  },
+  trashAssetsBeforeRuns: false,
+  video: false,
+});
