b7a346be751cfd090dadd51641d4471ded920d1e
Andrés Rivera Toro <andres.rivera.toro@gmail.com>
2023-10-28 14:32:32 -0400

    chore: restructure HTML for SR (#1395)

--- src/components/exploreApiCard/ExploreApiCard.tsx
+++ src/components/exploreApiCard/ExploreApiCard.tsx
@@ -6,20 +6,21 @@ import { ExploreApiTags } from './ExploreApiTags';
 import './ExploreApiCard.scss';
 
 export const ExploreApiCard = ({ api }: { api: APIDescription }): JSX.Element => (
-  <Link
-    to={`/explore/api/${api.urlSlug}`}
-    className={classNames(
-      'vads-u-text-decoration--none',
-      'vads-u-color--link-default',
-      'vads-u-width--full',
-    )}
-  >
-    <div className="explore-api-card-container">
+  <div className="explore-api-card-container">
+    <Link
+      to={`/explore/api/${api.urlSlug}`}
+      className={classNames(
+        'vads-u-text-decoration--none',
+        'vads-u-color--link-default',
+        'vads-u-width--full',
+        'explore-api-link',
+      )}
+    >
       <span className={classNames('vads-u-font-size--lg', 'vads-u-font-weight--bold')}>
         {api.name}
       </span>
-      <p className={classNames('vads-u-color--base')}>{api.description}</p>
-      <ExploreApiTags api={api} />
-    </div>
-  </Link>
+    </Link>
+    <p className={classNames('vads-u-color--base')}>{api.description}</p>
+    <ExploreApiTags api={api} />
+  </div>
 );
