02134af100d0b769635c0cae0d7cfd800da81a3e
Andres Rivera Toro <andres.rivera.toro@gmail.com>
2023-10-30 20:57:37 -0400

    chore: remove shadow dom tests

--- src/components/veteranResources/VeteranResources.test.tsx
+++ src/components/veteranResources/VeteranResources.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import 'jest';
-import { fireEvent, render, waitFor, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { VeteranResources } from './VeteranResources';
 
 describe('VeteranResources', () => {
@@ -12,57 +12,14 @@ describe('VeteranResources', () => {
     expect(modalButton).toHaveTextContent('Veteran resources');
   });
 
-  it('checks open/close functionality of dialog works correctly using the X button.', async () => {
-    render(<VeteranResources />);
+  it('sets modal visible on button click', async () => {
+    const { container } = render(<VeteranResources />);
 
-    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+    const modal = container.querySelector('va-modal') as HTMLElement;
+    expect(modal).toHaveAttribute('visible', 'false');
 
-    const modalButton = screen.getByRole('button');
-    fireEvent.click(modalButton);
-    await waitFor(() => expect(screen.queryByRole('dialog')).toBeInTheDocument());
-
-    const dialogXButton = screen.getByLabelText('Close the undefined modal');
-    fireEvent.click(dialogXButton);
-    await waitFor(() => expect(screen.queryByRole('dialog')).not.toBeInTheDocument());
-  });
-
-  it('checks open/close functionality of dialog works correctly using the Close button.', async () => {
-    render(<VeteranResources />);
-
-    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
-
-    const modalButton = screen.getByRole('button');
-    fireEvent.click(modalButton);
-    await waitFor(() => expect(screen.queryByRole('dialog')).toBeInTheDocument());
-
-    const dialogCloseButton = screen.getByText('Close');
-    fireEvent.click(dialogCloseButton);
-    await waitFor(() => expect(screen.queryByRole('dialog')).not.toBeInTheDocument());
-  });
-
-  it('checks open/close functionality of dialog works correctly clicking outside the modal.', async () => {
-    render(<VeteranResources />);
-
-    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
-
-    const modalButton = screen.getByRole('button');
-    fireEvent.click(modalButton);
-    await waitFor(() => expect(screen.queryByRole('dialog')).toBeInTheDocument());
-
-    fireEvent.click(document.body);
-    await waitFor(() => expect(screen.queryByRole('dialog')).not.toBeInTheDocument());
-  });
-
-  it('checks open/close functionality of dialog works correctly pressing the ESC key.', async () => {
-    render(<VeteranResources />);
-
-    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
-
-    const modalButton = screen.getByRole('button');
-    fireEvent.click(modalButton);
-    await waitFor(() => expect(screen.queryByRole('dialog')).toBeInTheDocument());
-
-    fireEvent.keyDown(modalButton, { charCode: 27, code: 'Escape', key: 'Escape' });
-    await waitFor(() => expect(screen.queryByRole('dialog')).not.toBeInTheDocument());
+    const openModalButton = screen.getByRole('button');
+    fireEvent.click(openModalButton);
+    expect(modal).toHaveAttribute('visible', 'true');
   });
 });
